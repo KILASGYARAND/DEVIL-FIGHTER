b5af736aeee35e879f6c66729c0c235427b84f2f
Naruto777549649 <sikrar026@gmail.com>
2025-04-05 17:40:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
+from pyrogram import Client
+from plugins import start, help, raid, gali, callbacks
 
+app = Client("devil fighter", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+# Add all handlers
+start.register(app)
+help.register(app)
+raid.register(app)
+gali.register(app)
+callbacks.register(app)
+
+app.run()
