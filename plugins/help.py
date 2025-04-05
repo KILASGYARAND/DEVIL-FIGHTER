aba19b2a5e2e552f3aa5a14c1c1953175c717e93
Naruto777549649 <sikrar026@gmail.com>
2025-04-05 17:48:28 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -1,1 +1,11 @@
+from pyrogram import Client, filters
 
+@Client.on_message(filters.command("help"))
+async def help_command(client, message):
+    text = "**Available Commands:**\n\n" \
+           "/raid - Start raiding the chat\n" \
+           "/stopraid - Stop the ongoing raid\n" \
+           "/target - Mention target user (Reply to user)\n" \
+           "/setgali - Set custom galiyaan\n" \
+           "/galilist - View current galiyaan"
+    await message.reply(text)
