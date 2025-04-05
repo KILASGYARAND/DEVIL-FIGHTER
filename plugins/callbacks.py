107432dd019fd23861db0fd52bc6ff6bc7009ca4
Naruto777549649 <sikrar026@gmail.com>
2025-04-05 17:49:37 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -1,1 +1,7 @@
+from pyrogram import Client, filters
+from pyrogram.types import CallbackQuery
 
+@Client.on_callback_query()
+async def callback_handler(client, callback_query: CallbackQuery):
+    if callback_query.data == "help":
+        await callback_query.message.edit("**This bot helps you raid with fun galiyaan! Use /raid to start.**")
