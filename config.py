48a8011a9710bf67b04c541628f3008f57c4a9da
Naruto777549649 <sikrar026@gmail.com>
2025-04-05 17:39:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
+API_ID = "YOUR_API_ID"
+API_HASH = "YOUR_API_HASH"
+BOT_TOKEN = "YOUR_BOT_TOKEN"
 
+OWNER_ID = 123456789  # Replace with your Telegram ID
+START_IMG = "https://example.com/image.jpg"  # Your bot image link
+JOIN_LINK = "https://t.me/yourchannel"
